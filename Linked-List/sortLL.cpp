cad1c85e777abc06f4ee5871ae1955cf23a7037d
ninja_shreyas <smekap3@gmail.com>
2025-08-24 23:44:10 +0530

    2-LL sums

---
+++ Linked-List/sortLL.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head || head->next==NULL) return head;
+        vector<int>l;
+        ListNode *temp = head;
+        while(temp!=NULL){
+            l.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(l.begin(),l.end());
+        temp = head;
+        int i=0;
+        while(temp!=NULL){
+            temp->val = l[i++];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
